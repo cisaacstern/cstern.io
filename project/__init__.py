c91cd0452b62b597d3948a9a18fb8f41c34b3fcc
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-02-05 16:03:12 -0800

    badges, css, copy improvemnts

--- project/__init__.py
+++ project/__init__.py
@@ -2,11 +2,11 @@ import os
 import json
 import io
 
+import markdown
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -14,18 +14,24 @@ def home():
 
 @app.route('/projects')
 def projects():
-    data = get_static_json("static/projects/projects.json")['projects']
-    data.sort(key=order_projects_by_weight, reverse=True)
+    data = get_static_json("static/projects/projects.json")
+    projects = data['projects']
+    projects.sort(key=order_projects_by_weight, reverse=True)
 
     tag = request.args.get('tags')
     if tag is not None:
-        data = [project for project in data if tag.lower() in [project_tag.lower() for project_tag in project['tags']]]
+        projects = [p for p in projects if tag.lower() in [tag.lower() for tag in p['tags']]]
 
-    return render_template('projects.html', projects=data, tag=tag)
+    badgebase = data['badge']['base']
+    badgetail = data['badge']['sm_tail']
+
+    return render_template('projects.html', projects=projects, tag=tag, 
+                            badgebase=badgebase, badgetail=badgetail)
 
 @app.route('/projects/<title>')
 def project(title):
-    projects = get_static_json("static/projects/projects.json")['projects']
+    data = get_static_json("static/projects/projects.json")
+    projects = data['projects']
 
     in_project = next((p for p in projects if p['link'] == title), None)
 
@@ -37,10 +43,25 @@ def project(title):
     # load html if the json file doesn't contain a description
     if 'description' not in selected:
         path = "projects"
-        selected['description'] = io.open(get_static_file(
-            'static/%s/%s/%s.html' % (path, selected['link'], selected['link'])), "r", encoding="utf-8").read()
-    return render_template('project.html', project=selected)
+        md = io.open(get_static_file(
+            'static/%s/%s/%s.md' % (path, selected['link'], selected['link'])), "r", encoding="utf-8").read()
+        selected['description'] = markdown.markdown(md)
 
+    name = selected['name']
+    last_commit = data['badge']['base'] + name + data['badge']['lg_tail']
+    
+    return render_template('project.html', project=selected, last_commit=last_commit)
+
+@app.route('/goals')
+def goals():
+    data = get_static_json("static/giftgoals/giftgoals.json")['giftgoals']
+    data.sort(key=order_projects_by_weight, reverse=True)
+
+    tag = request.args.get('tags')
+    if tag is not None:
+        data = [project for project in data if tag.lower() in [project_tag.lower() for project_tag in project['tags']]]
+
+    return render_template('giftgoals.html', giftgoals=data, tag=tag)
 
 def get_static_file(path):
     site_root = os.path.realpath(os.path.dirname(__file__))
---
+++ project/static/projects/when-rad/when-rad.md
@@ -0,0 +1,21 @@
+## motivation
+
+how it started
+
+## what works
+
+how its going
+
+> **highlight**
+
+>    code snippet
+
+## room to grow
+
+> **big picture**
+
+> the big theme
+
+## further reading
+
+You might also be intersted in (link)[github.com], which explores x, y, and z. Or maybe (link)[github.com] which looks at it another way.
